969a43ed76f300acac79bf4dc7a2027e590698dd
BarbaraMarcelino <barbara.marcelino@campus.ul.pt>
2020-06-18 21:12:52 +0100

    Menu mobile 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 const iconmenu = document.querySelector('.iconmenu');
 const navigation = document.querySelector('.navigation');
-const cross = document.querySelector('.iconcross');
+const iconcross = document.querySelector('.iconcross');
 if (iconmenu) {
     iconmenu.addEventListener('click', (e) => {
         e.preventDefault();
         navigation.classList.add("open");
     });
 }
-if (cross) {
-    cross.addEventListener('click', (e) => {
+if (iconcross) {
+    iconcross.addEventListener('click', (e) => {
         e.preventDefault();
         navigation.classList.remove("open");
     });
